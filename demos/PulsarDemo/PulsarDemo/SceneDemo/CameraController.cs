15e4e8757092d2394e0beeee9d0ab5cbdb846b28
TakuProjects <taku.projects@gmail.com>
2013-05-16 18:28:44 +0200

    Update demo
    Demo updated to use Input module of Pulsar

--- demos/PulsarDemo/PulsarDemo/SceneDemo/CameraController.cs
+++ demos/PulsarDemo/PulsarDemo/SceneDemo/CameraController.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 
+using Pulsar.Input;
 using Pulsar.Graphics;
 using Pulsar.Graphics.SceneGraph;
 using Pulsar.Graphics.Rendering;
@@ -18,37 +19,92 @@ namespace PulsarDemo.SceneDemo
     {
         #region Fields
 
+        private const string solarController = "SolarController";
+        private const string forwardName = "Forward";
+        private const string backName = "Back";
+        private const string rightName = "Right";
+        private const string leftName = "Left";
+        private const string horizontalName = "Horizontal";
+        private const string verticalName = "Vertical";
+
         private readonly float speed = 20.0f;
+        private readonly float lookSpeed = 75.0f;
         private CameraStyle camStyle = CameraStyle.FREE;
-        private Camera cam = null;
-        private bool goForward = false;
-        private bool goBack = false;
-        private bool goRight = false;
-        private bool goLeft = false;
+        private InputManager input;
+        private PlayerInput player;
+        private VirtualInput currentInput;
+        private Camera cam;
+        private bool goForward;
+        private bool goBack;
+        private bool goRight;
+        private bool goLeft;
 
         #endregion
 
         #region Constructors
 
-        public CameraController(Camera cam)
+        public CameraController(Camera cam, InputManager input)
         {
             this.cam = cam;
+            this.input = input;
+            this.input.CreatePlayer(0);
+            this.player = this.input.GetPlayer(0);
+            this.player.CreateContext(CameraController.solarController);
+            this.player.SetCurrentContext(CameraController.solarController);
+            this.currentInput = this.player.GetContext(CameraController.solarController);
+            this.CreateInputMapping();
         }
 
         #endregion
 
         #region Methods
 
+        public void CreateInputMapping()
+        {
+            Button forward = new Button() { Name = CameraController.forwardName };
+            Button back = new Button() { Name = CameraController.backName };
+            Button right = new Button() { Name = CameraController.rightName };
+            Button left = new Button() { Name = CameraController.leftName };
+            Axis horizontal = new Axis() { Name = CameraController.horizontalName, Inverse = true };
+            Axis vertical = new Axis() { Name = CameraController.verticalName, Inverse = true };
+            
+#if WINDOWS
+            forward.AddButton(new DigitalButton(Keys.Z), 0);
+            forward.AddButton(new DigitalButton(Keys.Up), 1);
+            back.AddButton(new DigitalButton(Keys.S), 0);
+            back.AddButton(new DigitalButton(Keys.Down), 1);
+            right.AddButton(new DigitalButton(Keys.D), 0);
+            right.AddButton(new DigitalButton(Keys.Right), 1);
+            left.AddButton(new DigitalButton(Keys.Q), 0);
+            left.AddButton(new DigitalButton(Keys.Left), 1);
+            horizontal.AddButton(new AnalogButton(MouseAnalogButtons.MouseX), 0);
+            vertical.AddButton(new AnalogButton(MouseAnalogButtons.MouseY), 0);
+#endif
+            forward.AddButton(new DigitalButton(Buttons.LeftThumbstickUp), 2);
+            back.AddButton(new DigitalButton(Buttons.LeftThumbstickDown), 2);
+            right.AddButton(new DigitalButton(Buttons.LeftThumbstickRight), 2);
+            left.AddButton(new DigitalButton(Buttons.LeftThumbstickLeft), 2);
+            horizontal.AddButton(new AnalogButton(GamePadAnalogButtons.RightThumbStickX), 1);
+            vertical.AddButton(new AnalogButton(GamePadAnalogButtons.RightThumbStickY), 1);
+
+            this.currentInput.AddButton(forward);
+            this.currentInput.AddButton(back);
+            this.currentInput.AddButton(right);
+            this.currentInput.AddButton(left);
+            this.currentInput.AddAxis(horizontal);
+            this.currentInput.AddAxis(vertical);
+        }
+
         public void Tick(GameTime time)
         {
             this.UpdateKey();
-            this.ComputeFrame();
+            this.ComputeFrame(time);
         }
 
-        private void ComputeFrame()
+        private void ComputeFrame(GameTime time)
         {
             this.ProcessMovement();
-            this.ProcessMouse();   
+            this.ProcessMouse(time);   
         }
 
         private void ProcessMovement()
@@ -65,35 +121,34 @@ namespace PulsarDemo.SceneDemo
             }
         }
 
-        private void ProcessMouse()
+        private void ProcessMouse(GameTime time)
         {
             if (this.camStyle == CameraStyle.FREE)
             {
-                Vector2 diff = Input.Instance.GetMousePositionDiff();
-                this.cam.Yaw(MathHelper.ToRadians(diff.X) * 0.15f);
-                this.cam.Pitch(MathHelper.ToRadians(diff.Y) * 0.15f);
+                float xValue = this.currentInput.GetAxis(CameraController.horizontalName).Value;
+                float yValue = this.currentInput.GetAxis(CameraController.verticalName).Value;
+                float yaw = MathHelper.Clamp(xValue, -1.0f, 1.0f) * (this.lookSpeed * (float)time.ElapsedGameTime.TotalSeconds);
+                float pitch = MathHelper.Clamp(yValue, -1.0f, 1.0f) * (this.lookSpeed * (float)time.ElapsedGameTime.TotalSeconds);
+                this.cam.Yaw(MathHelper.ToRadians(yaw));
+                this.cam.Pitch(MathHelper.ToRadians(pitch));
             }
-
-            //Input.Instance.ResetMouse();
         }
 
         private void UpdateKey()
         {
             if (this.camStyle == CameraStyle.FREE)
             {
-                Input inHandler = Input.Instance;
+                if (this.currentInput.GetButton(CameraController.forwardName).IsDown) this.goForward = true;
+                else if (this.currentInput.GetButton(CameraController.forwardName).IsUp) this.goForward = false;
 
-                if ((inHandler.IsKeyDown(Keys.Up)) || (inHandler.IsKeyDown(Keys.Z))) this.goForward = true;
-                else if ((inHandler.IsKeyUp(Keys.Up)) || (inHandler.IsKeyUp(Keys.Z))) this.goForward = false;
+                if (this.currentInput.GetButton(CameraController.backName).IsDown) this.goBack = true;
+                else if (this.currentInput.GetButton(CameraController.backName).IsUp) this.goBack = false;
 
-                if ((inHandler.IsKeyDown(Keys.Down)) || (inHandler.IsKeyDown(Keys.S))) this.goBack = true;
-                else if ((inHandler.IsKeyUp(Keys.Up)) || (inHandler.IsKeyUp(Keys.S))) this.goBack = false;
+                if (this.currentInput.GetButton(CameraController.rightName).IsDown) this.goRight = true;
+                else if (this.currentInput.GetButton(CameraController.rightName).IsUp) this.goRight = false;
 
-                if ((inHandler.IsKeyDown(Keys.Right)) || (inHandler.IsKeyDown(Keys.D))) this.goRight = true;
-                else if ((inHandler.IsKeyUp(Keys.Right)) || (inHandler.IsKeyUp(Keys.D))) this.goRight = false;
-
-                if ((inHandler.IsKeyDown(Keys.Left)) || (inHandler.IsKeyDown(Keys.Q))) this.goLeft = true;
-                else if ((inHandler.IsKeyUp(Keys.Left)) || (inHandler.IsKeyUp(Keys.Q))) this.goLeft = false;
+                if (this.currentInput.GetButton(CameraController.leftName).IsDown) this.goLeft = true;
+                else if (this.currentInput.GetButton(CameraController.leftName).IsUp) this.goLeft = false;
             }
         }
 
