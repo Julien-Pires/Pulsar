24001cd4a694d97cdd2179557db34f6396c2e3ea
Takumii <piresjulien95@gmail.com>
2013-08-18 16:52:32 +0200

    Fixes #118
    Rename IInputProvider .cs to IInputService.cs
    IGraphicsEngineService is public

---
+++ source/Pulsar/Input/IInputService.cs
@@ -0,0 +1,17 @@
+﻿namespace Pulsar.Input
+{
+    /// <summary>
+    /// Interface for input service provider
+    /// </summary>
+    public interface IInputService
+    {
+        #region Properties
+
+        /// <summary>
+        /// Get an InputManager instance
+        /// </summary>
+        InputManager Input { get; }
+
+        #endregion
+    }
+}
