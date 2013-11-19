6142534b07afef6080a8a97126e79ad2fe72ade4
Takumii <piresjulien95@gmail.com>
2013-11-19 16:15:29 +0100

    Fixes #173

--- source/Pulsar/Graphics/Rendering/Window.cs
+++ source/Pulsar/Graphics/Rendering/Window.cs
@@ -69,6 +69,7 @@ namespace Pulsar.Graphics.Rendering
         {
             PreferredResizeRule = ResizeRule.None;
             Initialize();
+            CreateViewport(0);
         }
 
         #endregion
